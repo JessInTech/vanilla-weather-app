18ec1d68c2fe84dcb1d0116b9b226704cebeaa9a
Jess in Tech <JessInTech@protonmail.com>
2022-12-09 16:09:39 -0700

    Showing Current Temp

--- src/app.js
+++ src/app.js
@@ -1,1 +1,20 @@
-alert("hello world")
+function displayTemperature(response) {
+    console.log(response.data)
+    let temperatureElement = document.querySelector("#temperature");
+    let cityElement = document.querySelector("#city");
+    let descriptionElement = document.querySelector("#description");
+    let humidityElement = document.querySelector("#humidity");
+    let windElement = document.querySelector("#wind");
+    let timeElement = document.querySelector("#time");
+    temperatureElement.innerHTML = Math.round(response.data.main.temp);
+    cityElement.innerHTML = response.data.name;
+    descriptionElement.innerHTML = response.data.weather[0].description;
+    humidityElement.innerHTML = response.data.main.humidity;
+    windElement.innerHTML = Math.round(response.data.main.wind.speed);
+    windElement.innerHTML = Math.round(response.data.main.wind.speed);
+}
+
+let apiKey = "7c8163af06b96db1c9990d6054e4d0ee";
+let apiUrl = "https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}&units=metric"
+
+axios.get(apiUrl).then(displayTemperature);

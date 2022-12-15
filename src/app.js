2e9dcf9dc18004325ba5b58710906a707b1f8d00
Jess in Tech <JessInTech@protonmail.com>
2022-12-15 12:40:45 -0700

    Update app.js

--- src/app.js
+++ src/app.js
@@ -42,7 +42,7 @@ function displayTemperature(response) {
     dateElement.innerHTML = formatDate(response.data.time);
     iconElement.setAttribute(
         "src",
-        "http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${response.data.weather[0].icon)@rain-day.png",
+        "http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${response.data.weather[0}.icon@rain-day.png",
     );
     iconElement.setAttribute("alt", response.data.condition.description);
 

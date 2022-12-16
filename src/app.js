d76246e69bdb1cd6166b64d0c33a64bc0e7eefca
Jess in Tech <JessInTech@protonmail.com>
2022-12-15 17:34:17 -0700

    rounding conversions

--- src/app.js
+++ src/app.js
@@ -64,14 +64,19 @@ function handleSubmit(event) {
 
 function displayFahrenheitTemperature(event){
     event.preventDefault();
-    let fahrenheitTemperature =  (celsiusTemperature * 9) / 5 + 32;
     let temperatureElement = document.querySelector("#temperature");
+    let fahrenheitTemperature =  (celsiusTemperature * 9) / 5 + 32;
     temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
 
 
 }
 
 
+function displayCelsiusTemperature(event){ 
+    event.preventDefault();
+    let temperatureElement = document.querySelector("#temperature");
+    temperatureElement.innerHTML = Math.round(celsiusTemperature);
+}
 
 let celsiusTemperature = null;
 
@@ -81,6 +86,9 @@ form.addEventListener("submit", handleSubmit);
 
 let fahreneheitLink = document.querySelector("#fahrenheit-link");
 fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
+
+let celsiusLink = document.querySelector("#celsius-link");
+celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
 
 search("New York");

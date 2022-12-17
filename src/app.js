4f497f7552589ac927df4d99e59211ed6c9478d7
Jess in Tech <JessInTech@protonmail.com>
2022-12-16 18:50:23 -0700

    Update app.js

--- src/app.js
+++ src/app.js
@@ -39,7 +39,7 @@ function displayForecast(response) {
             <div class="weather-forecast-date">${days[i]}</div>
             <img src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/rain-day.png"
             alt=""
-            width=46"
+            width=42"
             />
             <div class="weather-forecast-temperature">
             <span class="weather-forecast-temperature-max">${Math.round(

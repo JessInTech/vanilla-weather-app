b5d73df1a04f6a31de8dbc50cd290b855854ac62
Jess in Tech <JessInTech@protonmail.com>
2022-12-16 21:52:32 -0700

    Update app.js

--- src/app.js
+++ src/app.js
@@ -76,7 +76,7 @@ function displayForecast(response) {
 
 function getForecast(coordinates) {
   let apiKey = "7c8163af06b96db1c9990d6054e4d0ee";
-  let apiUrl = `https://api.openweathermap.org/data/3.0/onecall?lat=${coordinates.latitude}&lon=${coordinates.longitude}&keyid={apiKey}&units=imperial`;
+  let apiUrl = `https://api.openweathermap.org/data/3.0/onecall?lat=${response.data.coordinates.latitude}&lon=${response.data.coordinates.longitude}&keyid={apiKey}&units=imperial`;
   axios.get(apiUrl).then(displayForecast);
 }
 
@@ -106,7 +106,7 @@ function displayTemperature(response) {
 }
 
 function search(city) {
-//   let apiKey = "7c8163af06b96db1c9990d6054e4d0ee";
+  let apiKey = "7c8163af06b96db1c9990d6054e4d0ee";
   let apiUrl = `https://api.openweathermap.org/data/3.0/onecall?lat=${coordinates.latitude}&lon=${coordinates.longitude}&appid={apiKey}&units=imperial`;
 
   axios.get(apiUrl).then(displayTemperature);
@@ -132,4 +132,4 @@ function displayFahrenheitTemperature(event) {
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", handleSubmit);
 
-search("New York");
+// search("New York");

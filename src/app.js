8370c2726b4091d205e4383ed0dccdd359c6a53f
Jess in Tech <JessInTech@protonmail.com>
2022-12-16 18:47:32 -0700

    icon fix

--- src/app.js
+++ src/app.js
@@ -37,7 +37,7 @@ function displayForecast(response) {
     forecastHTML += `
             <div class="col-2">
             <div class="weather-forecast-date">${days[i]}</div>
-            <img src="https://shecodes-assets.s3.amazonaws.com/api/weather/icons/$[icon].png"
+            <img src="https://shecodes-assets.s3.amazonaws.com/api/weather/$[icons].png"
             alt=""
             width=46"
             />

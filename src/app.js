595fd1d927f0bc491db823f22b54ec51e42c3204
Jess in Tech <JessInTech@protonmail.com>
2022-12-16 22:56:24 -0700

    Update app.js

--- src/app.js
+++ src/app.js
@@ -23,47 +23,49 @@ function formatDate(timestamp) {
   let day = days[date.getDay()];
   return `${day} ${hours}:${minutes}`;
 }
-function displayForecast(response) {
-  let forecast = response.data.daily;
 
+
+function displayForecast(response) {
+//   let forecast = response.data.daily;
   let forecastElement = document.querySelector("#forecast");
-  
   let forecastHTML = `<div class="row">`;
   let days = [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday",
+    "Sun",
+    "Mon",
+    "Tue",
+    "Wed",
+    "Thur",
+    "Fri",
+    "Sat",
   ];
 
+  days.forEach(function(day){ 
+      forecastHTML += 
+              `
+              <div class="col-6">
+              <div class="weather-forecast-date">${day}</div>
+              <img 
+              src=""http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${response.data.weather[0].icon}",
+              alt=""
+              width="42"
+              />;
+              <div class="weather-forecast-temperature">
+              <span class="weather-forecast-temperature-max">${Math.round(
+                forecastDay.temperature.maximum
+              )}°</span>    
+              <span class="weather-forecast-temperature-min">${Math.round(
+                forecastDay.temperature.minimum
+              )}°</span>
+              </div>
+              </div>
+              `;
 
+  });
 
-  for (let forecastDay of forecast) {
-          let i = forecast.indexOf(forecastDay) 
+//   for (let forecastDay of forecast) {
+//           let i = forecast.indexOf(forecastDay) 
         
-        forecastHTML += 
-                `
-                <div class="col-2">
-                <div class="weather-forecast-date">${day[i]}</div>
-                <img 
-                src=""http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${response.data.weather[0].icon}",
-                alt=""
-                width="42"
-                />;
-                <div class="weather-forecast-temperature">
-                <span class="weather-forecast-temperature-max">${Math.round(
-                  forecastDay.temperature.maximum
-                )}°</span>    
-                <span class="weather-forecast-temperature-min">${Math.round(
-                  forecastDay.temperature.minimum
-                )}°</span>
-                </div>
-                </div>
-                `;
-  }
+  
             forecastHTML += `</div>`;
             forecastElement.innerHTML = forecastHTML;
             
